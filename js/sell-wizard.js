eba5af11e6d3f82d0d9977a2c9e6f8a6d09364a5
miji7575 <mipark7575@gmail.com>
2022-12-28 10:34:59 +0900

    miji sell-index 추가

--- js/sell-wizard.js
+++ js/sell-wizard.js
@@ -46,10 +46,9 @@ window.addEventListener("load", function () {
 
 
     quitReg.addEventListener("click", function () {
-        document.querySelector("#one").classList.add("d-none");
-        document.querySelector("#two").classList.remove("d-none");
-        wizard[0].classList.remove("first");
-        wizard[1].classList.add("second");
+
+        //==================페이지에서 나가야됨-==================//
+
     })
     secondBack.addEventListener("click", function () {
         document.querySelector("#two").classList.add("d-none");
@@ -73,6 +72,90 @@ window.addEventListener("load", function () {
 
 
 
+    //=======================================================================//
+    // ====================    이미지 넣기 (1번째 페이지)    ================//
+    //=======================================================================//
+
+
+
+    const imgInput = document.querySelector(".img-input");
+    const imgInputAfter = document.querySelector(".img-input-after");
+    const fileInput = document.querySelector(".file-input");
+    let inputBox = document.querySelector(".input-box")
+    let newimgInputAfter;
+
+
+
+    let imgNum = this.document.querySelector(".img-num")
+    let imgIndex = 0;
+    imgNum.innerHTML = imgIndex;
+
+
+
+
+
+
+
+
+    imgInput.onclick = function () {
+        console.log(imgIndex);
+        if (imgIndex < 4) {
+            let event = new MouseEvent("click", {
+                'view': window,
+                'bubbles': true,
+                'cancelable': true
+            });
+
+            fileInput.dispatchEvent(event);
+
+
+
+        }
+    }
+
+
+
+
+    fileInput.oninput = function () {
+
+        let url = fileInput.files[0];
+
+        let reader = new FileReader();
+        reader.onload = (evt) => {
+
+
+            newimgInputAfter = imgInputAfter.cloneNode(true);
+            inputBox.insertBefore(imgInput, imgInputAfter)
+            inputBox.appendChild(newimgInputAfter);
+            imgInputAfter.src = evt.target.result;
+            imgInputAfter.classList.remove("d-none");
+            imgIndex++;
+            imgNum.innerHTML = imgIndex;
+
+
+        }
+
+
+
+        reader.readAsDataURL(url);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -86,6 +169,7 @@ window.addEventListener("load", function () {
     let supercate = document.querySelector(".supercategory-select");
     let subcate = document.querySelectorAll(".subcategory-select");
     let value;
+    let background = document.querySelector(".background");
 
 
 
@@ -93,6 +177,8 @@ window.addEventListener("load", function () {
     cate.onclick = function (e) {
         e.preventDefault();
         supercate.classList.remove("d-none");
+        background.classList.add("background-dark")
+        
     }
 
 
@@ -102,12 +188,15 @@ window.addEventListener("load", function () {
 
     supercate.onclick = function (e) {
 
-        if (e.target.classList.contains("exit"))
+        if (e.target.classList.contains("exit")){
+            background.classList.remove("background-dark");
             supercate.classList.add("d-none");
+        }
 
 
         if (e.target.checked) {
             value = e.target.getAttribute("id");
+            category.value = value;
         }
 
 
@@ -135,13 +224,16 @@ window.addEventListener("load", function () {
 
             subcate[supercateValue].onclick = function (e) {
 
-                if (e.target.classList.contains("exit"))
+                if (e.target.classList.contains("exit")){
                     subcate[supercateValue].classList.add("d-none");
+                    background.classList.remove("background-dark");
+                }
 
 
                 if (value != undefined && e.target.classList.contains("final")) {
+                    background.classList.remove("background-dark");
                     subcate[supercateValue].classList.add("d-none");
-                    category.value = value;
+                    category.value=value;
                 }
 
 
@@ -160,4 +252,87 @@ window.addEventListener("load", function () {
 
 
 
+
+    //=======================================================================//
+    // ====================    태그추가 (3번째 페이지)    ================//
+    //=======================================================================//
+
+
+    const tagInput = document.querySelector(".tag-input");
+    const tag = document.querySelector(".btn-tag");
+    const addBtn = document.querySelector(".btn-tag-add");
+    const tagBox = document.querySelector(".tag-box")
+    console.log(tag)
+    
+
+    let tagIndex = 0
+    let blankPattern = /^\s+|\s+$/g;
+
+
+    tagInput.onclick = function () {
+        if (tagIndex == 5) {
+            alert("태그는 5개까지만 추가 가능합니다.")
+        }
+
+
+    }
+
+
+
+    addBtn.onclick = function () {
+        if(!tagInput.value|| (tagInput.value.replace(blankPattern, '' ) == "")){
+            alert("공백이 입력되었습니다.")
+            return;
+        }
+        console.log(tagIndex);
+        if (tagIndex < 5) {
+
+
+            newTag = tag.cloneNode(true);
+            tagBox.appendChild(newTag);
+            tagBox.insertBefore(tag, null)
+
+
+
+            tag.innerHTML = tagInput.value;
+
+            tag.classList.remove("d-none")
+            tagIndex++;
+            tagInput.value = "";
+            tagInput.setAttribute(Placeholder, "태그를 입력해주세요");
+            
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
+
+
+
